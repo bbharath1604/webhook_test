8472a9e3195b17d9894466ad9f669fde91b208d5
bbharath1604 <77154824+bbharath1604@users.noreply.github.com>
2021-02-05 16:59:16 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 import time
 
 if __name__ == '__main__':
-	print('hi commit webhook test! \n')
+	print('hi commit webhook trigger test! \n')
 	time.sleep(1)
-	print('bye webhook worked :) \n')
+	print('bye webhook trigger worked :) \n')
