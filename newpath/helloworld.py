ceba1235b602028c4272b072ba08829cd33275bb
bbharath1604 <77154824+bbharath1604@users.noreply.github.com>
2021-02-05 17:23:19 +0100

    Update helloworld.py

--- newpath/helloworld.py
+++ newpath/helloworld.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
 	print('hi building from a docker placed not directly under repo! :) \n')
 	time.sleep(1)
 	print('bye webhook trigger worked :) \n')
-	print(':)')
+	print(':) :)')
